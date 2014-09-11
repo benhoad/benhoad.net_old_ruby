c28f874cc7f44b1cd3ef5650097a542fa832c6d0
Ben Hoad <ben@benhoad.net>
2014-09-11 18:11:35 +1000

    updated konami code

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,6 +1,7 @@
 cheet('↑ ↑ ↓ ↓ ← → ← → b a', function () {
   document.getElementById('profile').style.background = "url('/images/heman.gif')";
-  document.getElementById('background').style.background = "url('/images/heman2.gif') 50% 50%";
+  // document.getElementById('background').style.background = "url('/images/heman2.gif') 50% 50%";
+  document.getElementById('background').innerHTML = '<iframe width="100%" height="100%" src="//www.youtube.com/embed/w5FUu6GM__U?autoplay=1" frameborder="0" allowfullscreen></iframe>'
 });
 
 
@@ -8,33 +9,33 @@ $(document).ready(function(){
   $("body").bind("keydown", function (e) {
     if (e.keyCode == 38) {
       //up
-      $('.content-wrapper').css('-webkit-transform', 'translate3d(0,-20px,0)');
+      $('.content-wrapper').css('-webkit-transform', 'rotateX(10deg)');
     }
 
     if (e.keyCode == 40) {
       //down
-      $('.content-wrapper').css('-webkit-transform', 'translate3d(0,20px,0)');
+      $('.content-wrapper').css('-webkit-transform', 'rotateX(-10deg)');
     }
 
     if (e.keyCode == 37) {
       //left
-      $('.content-wrapper').css('-webkit-transform', 'translate3d(-20px,0,0)');
+      $('.content-wrapper').css('-webkit-transform', 'rotateY(-10deg)');
     }
 
     if (e.keyCode == 39) {
       //right
-      $('.content-wrapper').css('-webkit-transform', 'translate3d(20px,0,0)');
+      $('.content-wrapper').css('-webkit-transform', 'rotateY(10deg)');
     }
 
     if (e.keyCode == 65) {
       //a
-      $('.content-wrapper').css('-webkit-transform', 'scale(1.1,1.1)');
+      $('.content-wrapper').css('-webkit-transform', 'scale(0.9,0.9)');
     }
 
 
     if (e.keyCode == 66) {
       //b
-      $('.content-wrapper').css('-webkit-transform', 'scale(1.1,1.1)');
+      $('.content-wrapper').css('-webkit-transform', 'scale(0.9,0.9)');
 
     }
   });
